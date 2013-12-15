a2a7db648ffd7146afcd37f85d761537b865f8bd
Nejuf <nejuf1@gmail.com>
2013-12-15 13:21:45 -0800

    Implements name restrictions

--- chat.js
+++ chat.js
@@ -1,70 +1,136 @@
 var net = require("net");
 
-Array.prototype.remove = function(e) {
-  for (var i = 0; i < this.length; i++) {
-    if (e == this[i]) { return this.splice(i, 1); }
-  }
-};
+var RESERVED_NAMES = ['user', 'users', 'self', 'bot', 'mod', 'moderator', 'admin', 'administrator', 'system'];
 
 function Client(stream) {
   this.name = null;
   this.stream = stream;
+  this.message = "";
 }
 
 var clients = [];
 
+console.log("Creating server...");
 var server = net.createServer(function (stream) {
+	console.log("Server connected.");
+
   var client = new Client(stream);
   clients.push(client);
 
   stream.setTimeout(0);
   stream.setEncoding("utf8");
 
-  stream.addListener("connect", function () {
-    stream.write("Welcome, enter your username:\n");
-  });
+  var sendMessage = function(message, target){
+  	if(!message){
+  		message = client.message + "\r";
+  		client.message = "";
+  	}
+  	switch(target){
+  		case "self":
+  			client.stream.write(message + "\r\n");
+  		break;
+  		default:
+	  		clients.forEach(function(c) {
+	        c.stream.write("\r\033[K" + client.name + ": " + message + "\r\n" + c.message);
+	      });
+	    break;
+  	}
+  };
+
+  stream.write("Welcome to Chat!\r\n Use /quit, /end, or /exit to leave chat.\r\n\nPlease enter your username:\r\n");
+  var enteringUsername = true;
 
   stream.addListener("data", function (data) {
-    if (client.name == null) {
-      client.name = data.match(/\S+/);
-      stream.write("===========\n");
-      clients.forEach(function(c) {
-        if (c != client) {
-          c.stream.write(client.name + " has joined.\n");
-        }
-      });
-      return;
-    }
+  	if (data.match(/\n/)){
+  		stream.write('\033[1A' + '\r\033[K');//clears previous line
+  		
+  		if(enteringUsername){
+  			name = client.message;
 
-    var command = data.match(/^\/(.*)/);
-    if (command) {
-      if (command[1] == 'users') {
-        clients.forEach(function(c) {
-          stream.write("- " + c.name + "\n");
-        });
-      }
-      else if (command[1] == 'quit') {
-        stream.end();
-      }
-      return;
-    }
+  			if(nameReserved(name)){
+  				client.message = "";
+  				sendMessage("The name \"" + name + "\" is reserved.  Please select another.", "self");
+  			}
+  			else if(nameTaken(name)){
+  				client.message = "";
+					sendMessage("The name \"" + name + "\" is already taken by another user.", "self");
+  			}
+  			else{
+  				enteringUsername = false;
+	  			client.name = client.message;
+	  			client.message = "";
 
-    clients.forEach(function(c) {
-      if (c != client) {
-        c.stream.write(client.name + ": " + data);
-      }
-    });
+	  			sendMessage(client.name + " has joined the room.");
+  			}
+  		}
+  		else {
+  			var command = client.message.match(/^\/(.*)/);
+
+		    if (command) {
+		    	switch(command[1].toLowerCase()){
+		    		case 'users':
+			  			client.message = "";
+			    		clients.forEach(function(c) {
+			          sendMessage("- " + c.name, 'self');
+			        });
+		    		break;
+		    		case 'quit':
+		    		case 'end':
+		    		case 'exit':
+			  			client.message = "";
+			        stream.end();
+			        return;
+		    		break;
+		    		default:
+		    			message = client.message;
+		    			client.message = "";
+		    			sendMessage("Unrecognized command: " + client.message, 'self');
+		    		break;
+		    	}
+		    }
+		    else if (client.message.length){
+		  		sendMessage();
+		  	}
+	  	}
+  	}
+  	else{
+  		client.message += data;
+  	}
+
   });
 
   stream.addListener("end", function() {
-    clients.remove(client);
+    clients.splice(clients.indexOf(client), 1);
 
     clients.forEach(function(c) {
-      c.stream.write(client.name + " has left.\n");
+      c.stream.write(client.name + " has left.\r\n");
     });
 
     stream.end();
   });
 });
 
-server.listen(7000);
+var nameTaken = function(name){
+	var len = clients.length;
+	for(var i = 0; i < len; i++){
+		if (clients[i].name !== null){	
+			if (clients[i].name.toLowerCase() == name.toLowerCase()){
+				return true;
+			}
+		}
+	}
+
+	return false;
+}
+
+var nameReserved = function(name){
+	if (RESERVED_NAMES.indexOf(name.toLowerCase()) >= 0){
+		return true;
+	}
+	return false;
+}
+
+console.log("Server created.");
+server.listen(7000, function(){
+	console.log("Chat server listening on localhost 7000.")
+});
